3dbad48a2d881137006a70187d8d221d5902f91a
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-17 15:51:15 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,15 @@
+
 def count_strings(array)
+  array.count do |element|
+    element.class == String
+  end
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
+emptys = 0
+  array.count do |element|
+element.class == []
+  emptys +=
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-end
+ end
